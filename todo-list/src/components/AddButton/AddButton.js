6243b9d9a56a25eaa6ad7b1bddd56498ea2e3260
olegmalyarenko <oi.malyarenko@gmail.com>
2020-05-19 11:08:23 +0300

    feat: add text input

--- todo-list/src/components/AddButton/AddButton.js
+++ todo-list/src/components/AddButton/AddButton.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import './AddButton.css';
 
 export default class AddButton extends Component {
+    state = {
+        label: ''
+    }
+    onLabelChange= (e) => {
+      this.setState({
+          label: e.target.value
+      });
+      console.log(this.state.label);
+
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+
+    }
     render() {
         const { addItem } = this.props;
         return (
-            <div className='item-add-form'>
+            <form className='item-add-form d-flex'
+                  onSubmit={this.onSubmit}>
+            <input type="text" 
+                   className="form-control"
+                   onChange={this.onLabelChange}
+                   placeholder="What needs to be done"/>     
+
             <button
             className='btn btn-outline-secondary' 
-            onClick= {() =>   addItem('Hello fresh') }
             >Add</button>
-            </div>
+            </form>
         )
     }
 }
