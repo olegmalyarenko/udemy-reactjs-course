b47c19a23c3eb1f35525384c1af8e8c9f2c8eadc
olegmalyarenko <oi.malyarenko@gmail.com>
2020-05-23 17:31:40 +0300

    feat: add header

---
+++ swlib/src/components/app/App.js
@@ -0,0 +1,13 @@
+import React,{ Component} from 'react';
+import './App.css';
+import Header from '../header/Header';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Header/>
+        )
+    } 
+}
+
+
